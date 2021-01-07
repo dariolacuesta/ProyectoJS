91241a83dcb336756f49e4937cddd0b88a1c29be
dariolacuesta <71891850+dariolacuesta@users.noreply.github.com>
2021-01-07 19:29:38 -0300

    Add files via upload

---
+++ entidades.js
@@ -0,0 +1,44 @@
+/*El proyecto se basa en una pagina de venta de libros online. la idea es hacer un catalogo dividido por generos,top de ventas y ofertas y lograr tener un ''carrito'' el cual ir cargando las
+compras y mediante funciones ir modificando elementos del DOM que indiquen si hay stock/precio/etc*/
+
+class Libros{
+
+     
+    title= "";
+    writer="";
+    genre= "";
+    price = "";
+    stock =0;
+    id = randomId();    
+    constructor(id = null,title,writer,genre,price,stock)
+    {
+        this.id = randomId();
+        this.title = title;
+        this.writer = writer;
+        this.genre = genre;
+        this.price = price;
+        this.stock = stock;
+
+    }
+
+    priceWithIva(price)
+    {
+        let finalPrice = price * 1.21;  
+        return finalPrice;  
+    } 
+}
+
+ function randomId(){
+    id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    return id;}
+
+generos= ["Policial","Ciencia Ficcion","Infantil/Juvenil","Terror","Tradicionales"] 
+
+
+let book  = new Libros("1984","George Orwell",generos[1],"300$",12)
+let book2 = new Libros("Estudio en Escarlata","Edgar Allan Poe",generos[0],"400$",40)
+let book3 = new Libros("Harry Potter y la Orden del Fenix","J.K Rowling",generos[2],"600$",33)
+
+console.log(book);
+console.log(book2);
+console.log(book3);
